2317997ad097501b81c41ad9bb0208d4f23206b4
yuviSondulkar <80445051+yuviSondulkar@users.noreply.github.com>
2022-10-04 13:38:19 +0530

    Add files via upload

---
+++ Scripts/ChangeScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();  
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("NextLevel"))
+        {
+            Level();
+        }
+    }
+
+    // Update is called once per frame
+    private void Level()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
---
+++ Scripts/PlayerLife.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [SerializeField] private AudioSource deathSoundEffect;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();  
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+            
+        }
+    }
+
+    private void Die()
+    {
+        deathSoundEffect.Play();
+        anim.SetTrigger("death");
+        rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void RestartLevel()
+    {
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);    
+    }
+
+
+}
